389cbef21d575eb2c6353a595c61e4e87c534a2e
Siddhant Srinivas <117804794+siddhant-srinivas@users.noreply.github.com>
2023-09-28 14:29:55 +0530

    finished client.c

--- client.c
+++ client.c
@@ -20,77 +20,82 @@ int main(int argc,char* argv[]){
     int len;
     int msgid;
     if((key=ftok("server.c",'B'))==-1){
-        perror("ftok");
+        perror("ftok failed");
         exit(1);
     }
     if((msgid= msgget(key,PERMS))==-1){
-        perror("msgget");
+        perror("msgget failed");
         exit(1);
     }
-   char greeting[] = "1. Enter 1 to contact the Ping Server\n2. Enter 2 to contact the File Search Server\n3. Enter 3 to contact the File Word Count Server\n4. Enter 4 if this Client wishes to exit\n";
-   if(fork()==0){
-    int clientID;
-     
-    printf("Enter Client-ID: ");
-    scanf("%d",&clientID);
-   
-
-                // while(fgets(buf.mesg_text,sizeof(buf.mesg_text),stdin) !=NULL){
-                //     len = strlen(buf.mesg_text);
-                //     if(buf.mesg_text[len-1] == '\n') buf.mesg_text[len-1] = '\0';
-                
-                //}
-                
-               
-                
-    while(i!=4){
-        struct mesg_buffer buf;
-        struct mesg_buffer buf2;
-        printf("%s",greeting);
-        scanf("%d",&i);
-        getchar();
-        switch(i){
-            case 1:
-                    buf.mesg_type=1;
+    char greeting[] = "1. Enter 1 to contact the Ping Server\n2. Enter 2 to contact the File Search Server\n3. Enter 3 to contact the File Word Count Server\n4. Enter 4 if this Client wishes to exit\n\n";
+    pid_t pid = fork();
+    if(pid == -1){
+        perror("fork failed");
+        exit(EXIT_FAILURE);
+    }   
+    if(pid == 0){        
+        int clientID;  
+        printf("Enter Client-ID: ");
+        scanf("%d",&clientID);   
+        while(i!=4){
+            struct mesg_buffer buf;
+            struct mesg_buffer buf2;
+            printf("%s",greeting);
+            scanf("%d",&i);
+            getchar();
+            switch(i){
+                case 1:
+                    buf.mesg_type = 1;
                     strcpy(buf.mesg_text,"Hi\0");
-                     if(msgsnd(msgid,&buf,strlen(buf.mesg_text)+1,0)==-1){
-                    perror("msgsnd");}
+                    if(msgsnd(msgid,&buf,strlen(buf.mesg_text)+1,0)==-1){
+                        perror("msgsnd");
+                        exit(1);
+                    }
                     if(msgrcv(msgid,&buf2,sizeof(buf2.mesg_text),0,0)==-1){
                         perror("msgrcv");
                         exit(1);
                     }
-                    printf("%s\n",buf2.mesg_text);
+                    printf("%s\n\n",buf2.mesg_text);
                     break;
-            case 2:
-                buf.mesg_type = 2;
-                scanf("%[^\n]s",buf.mesg_text);
-                if(msgsnd(msgid,&buf,strlen(buf.mesg_text)+1,0)==-1){
-                    perror("msgsnd");
-                    exit(1);
-                }
-                if(msgrcv(msgid,&buf2,sizeof(buf2.mesg_text),0,0)==-1){
-                    perror("msgrcv");
-                    exit(1);
-                }
-                printf("%s\n",buf2.mesg_text);
-                break;
-             case 3:
-                buf.mesg_type = 3;
-                scanf("%[^\n]s",buf.mesg_text);
-                if(msgsnd(msgid,&buf,strlen(buf.mesg_text)+1,0)==-1){
-                    perror("msgsnd");
-                    exit(1);
-                }
-                if(msgrcv(msgid,&buf2,sizeof(buf2.mesg_text),0,0)==-1){
-                    perror("msgrcv");
-                    exit(1);
-                }
-                printf("%s\n",buf2.mesg_text);
-             break;
-         }
-     }
-   }else{
-     wait(NULL);
-    
+                case 2:
+                    buf.mesg_type = 2;
+                    printf("Enter the name of the file you are searching for: ");
+                    scanf("%[^\n]s",buf.mesg_text);
+                    if(msgsnd(msgid,&buf,strlen(buf.mesg_text)+1,0)==-1){
+                        perror("msgsnd");
+                        exit(1);
+                    }
+                    if(msgrcv(msgid,&buf2,sizeof(buf2.mesg_text),0,0)==-1){
+                        perror("msgrcv");
+                        exit(1);
+                    }
+                    printf("%s\n\n",buf2.mesg_text);
+                    break;
+                case 3:
+                    buf.mesg_type = 3;
+                    printf("Enter the name of the file whose word count you want: ");
+                    scanf("%[^\n]s",buf.mesg_text);
+                    if(msgsnd(msgid,&buf,strlen(buf.mesg_text)+1,0)==-1){
+                        perror("msgsnd");
+                        exit(1);
+                    }
+                    if(msgrcv(msgid,&buf2,sizeof(buf2.mesg_text),0,0)==-1){
+                        perror("msgrcv");
+                        exit(1);
+                    }
+                    printf("The word count of the file is: %s\n\n",buf2.mesg_text);
+                    break;
+                case 4:
+                    while(wait(NULL) > 0){}
+                    printf("Client exited\n");
+                    exit(EXIT_SUCCESS);
+                default:
+                    printf("Please enter a valid number: \n");
+                    break;
+            }
+        }
+    }
+    else{
+        wait(NULL);
     }
 }

0c398b4d6deeeb8d3f8c95b0d52aac9608291b60
Siddhant Srinivas <117804794+siddhant-srinivas@users.noreply.github.com>
2023-09-28 14:30:49 +0530

    finished server.c

--- server.c
+++ server.c
@@ -15,9 +15,7 @@ struct mesg_buffer{
 };
 int main(int argc,char* argv[]){
    
-
     struct mesg_buffer buf;
-
     int i;
     key_t key;
     int len;
@@ -33,16 +31,21 @@ int main(int argc,char* argv[]){
     printf("Ready to receive messages\n");
     while(1){
         if(msgrcv(msgid,&buf,sizeof(buf.mesg_text),0,0)==-1){
+            printf("%s", buf.mesg_text);
             perror("msgrcv");
             exit(1);
         }
-     
         switch(buf.mesg_type){
-            case 1:
-            if(fork()==0){
+        case 1:
+            pid_t pid1 = fork();
+            if(pid1 == -1){
+                perror("fork");
+                exit(EXIT_FAILURE);
+            }
+            if(pid1 == 0){
                 struct mesg_buffer buf2;
                 buf2.mesg_type= 1;
-                strcpy(buf2.mesg_text,"Hello\0");
+                strcpy(buf2.mesg_text,"Hello\0");       //Client
                 if(msgsnd(msgid,&buf2,strlen(buf2.mesg_text)+1,0)==-1){
                     perror("msgsnd");
                     exit(1);
@@ -51,90 +54,125 @@ int main(int argc,char* argv[]){
             }
             break;
         case 2:
-            if(fork()==0){
+            //printf("inside 2");
+            pid_t pid2 = fork();
+            if(pid2 == -1){
+                perror("fork");
+                exit(EXIT_FAILURE);
+            }
+            if(pid2 == 0){
                 int fd1[2];
                 if(pipe(fd1)==-1){
                     perror("pipe");
                     exit(1);
                 }
                 struct mesg_buffer buf3;
-
-                 if(fork()==0){
-                    close(fd1[0]);
-                    dup2(fd1[1],STDOUT_FILENO);
-                    
-                   execlp("find","find",".","-name",buf.mesg_text,NULL);
-                   close(fd1[1]);
+                pid_t cpid2 = fork();
+                if(cpid2 == -1){
+                    perror("fork");
+                    exit(EXIT_FAILURE);
+                }
+                //Grandchild
+                if(cpid2 == 0){
+                    close(fd1[0]);      //Closing read end
+                    dup2(fd1[1],STDOUT_FILENO);     //Redirecting stdout to write end
+                    execlp("find","find",".","-name",buf.mesg_text,NULL);
+                    close(fd1[1]);
                     perror("execlp");
                     exit(-1);
-                    }else{
-                        wait(NULL);
-                        char r2[20];
-                close(fd1[1]);
-                read(fd1[0],&r2,sizeof(r2));
-                close(fd1[0]);
-                for(int i=0;i<strlen(r2);i++){
-                    if(r2[i]=='\n') r2[i] = '\0';
                 }
-                printf("%s\n",r2);
-                   strcpy(buf3.mesg_text,r2);
-                    buf3.mesg_type = 2;
-                if(msgsnd(msgid,&buf3,strlen(buf3.mesg_text)+1,0)==-1){
-                    perror("msgsnd");
-                    exit(1);
-                 }
-                 exit(0);
+                //Child
+                else{
+                    wait(NULL);     //Ensures that file search finishes fully
+                    char r2[20];
+                    int x = 0;
+                    close(fd1[1]);
+                    if(read(fd1[0],&r2,sizeof(r2)) == 0){   //Read end is empty
+                        x = 1;
                     }
-                break;
-               
-            case 3:
-            
-                if(fork()==0){
-                    int fd[2];
-                    if(pipe(fd)==-1){
-                        perror("pipe");
+                    close(fd1[0]);
+                    for(int i=0;i<strlen(r2);i++){
+                        if(r2[i]=='\n') 
+                            r2[i] = '\0';   
+                        if((int)r2[i] > 128){
+                            x = 1;
+                            break;
+                        }
+                    }
+                    if(x == 0){
+                        char f[50] = "File found: ";
+                        strcat(f, r2);
+                        strcpy(buf3.mesg_text,f);                       
+                    }
+                    else{
+                        char nf[20] = "File not found";
+                        strcpy(buf3.mesg_text, nf);
+                    }
+                    buf3.mesg_type = 2;
+                    if(msgsnd(msgid,&buf3,strlen(buf3.mesg_text)+1,0)==-1){
+                        perror("msgsnd");
                         exit(1);
                     }
+                    exit(0);
+                }
+            }
+            break; 
+        case 3:
+            pid_t pid3 = fork();
+            if(pid3 == -1){
+                perror("fork");
+                exit(EXIT_FAILURE);
+            }
+            if(pid3 == 0){
+                int fd[2];
+                if(pipe(fd)==-1){
+                    perror("pipe");
+                    exit(1);
+                }
                 struct mesg_buffer buf4;
                 int a;
-                    if(fork()==0){
-                        dup2(fd[1],STDOUT_FILENO);
-                    
-                   execlp("wc","wc","-w",buf.mesg_text,NULL);
-                   close(fd[1]);
+                pid_t cpid3 = fork();
+                if(cpid3 == -1){
+                    perror("fork");
+                    exit(EXIT_FAILURE);
+                }
+                if(cpid3 == 0){
+                    //Insert code for detecting file not found
+                    dup2(fd[1],STDOUT_FILENO);
+                    execlp("wc","wc","-w",buf.mesg_text,NULL);
+                    close(fd[1]);
                     perror("execlp");
                     exit(-1);
-                    }else{
-                        wait(NULL);
-                        char r[20];
-                close(fd[1]);
-                read(fd[0],&r,sizeof(r));
-                close(fd[0]);
-                for(int i=0;i<strlen(r);i++){
-                    if(r[i]==' ') r[i] = '\0';
                 }
-                printf("%s\n",r);
-                   strcpy(buf4.mesg_text,r);
-                    buf4.mesg_type = 3;
-                if(msgsnd(msgid,&buf4,strlen(buf4.mesg_text)+1,0)==-1){
-                    perror("msgsnd");
-                    exit(1);
-                 }
-                 exit(0);
+                else{
+                    wait(NULL);
+                    char r[20];
+                    close(fd[1]);
+                    read(fd[0],&r,sizeof(r));
+                    close(fd[0]);
+                    for(int i=0;i<strlen(r);i++){
+                        if(r[i]==' ') r[i] = '\0';
                     }
+                    printf("%s\n",r);
+                    strcpy(buf4.mesg_text,r);
+                    buf4.mesg_type = 3;
                     
-                    
-                    
+                    if(msgsnd(msgid,&buf4,strlen(buf4.mesg_text)+1,0)==-1){
+                        perror("msgsnd");
+                        exit(1);
+                    }
+                    exit(0);
+                }
             }
             break;
+        case 4:
+            while(wait(NULL) > 0){};
+            if(msgctl(msgid, IPC_RMID, NULL) == -1){
+                perror("msgctl");
+                exit(1);
             }
-        }
-          
+            printf("Main server terminated\n");
+            exit(0);
+        } 
     }
-    wait(NULL);
-     if(msgctl(msgid,IPC_RMID,NULL)==-1){
-                    perror("msgctl");
-                    exit(1);
-                }
- 
 }
